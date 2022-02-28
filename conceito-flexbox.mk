c6f7e7a1e8ef0a7aa0837bdfe081c252d3ab906f
zelito37 <zelito37@gmail.com>
2022-02-28 17:07:24 -0300

    atualizaçao v-1.001

---
+++ conceito-flexbox.mk
@@ -0,0 +1,6 @@
+##     Conceito de flexbox
+
+O flexible box module, geralmente chamado de flexbox, foi projetado tanto como um modelo de layout unidimensional quanto um m�todo capaz de organizar especialmente os elementos em uma interface, al�m de possuir capacidades avan�adas de alinhamento.
+Quando se descreve o flebox como sendo unidimensional, enfatiza-se o fato que ele lida com o layout em uma dimens�o de cada vez � seja uma linha ou uma coluna. Isto pode ser comprado com modelo bidimensional de CSS - Layout de Grade, que permite o controle simult�neo  
+das colunas e linhas.
+
